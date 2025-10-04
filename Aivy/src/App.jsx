d891e0bc77c703ece3f70317375f4b5eb5a9a553
Chetan Krishna Palicherla <52138415+ChetanKrishna07@users.noreply.github.com>
2025-10-04 16:00:52 -0400

    initial commit

---
+++ Aivy/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Flashcard } from './Components/Flashcard'
+import { Navigation } from './Components/Navigation'
+import { Pet } from './Components/Pet'
+import { Store } from './Components/Store'
+
+function App() {
+  return (
+    <Router>
+      <main>
+        <Routes>
+          <Route path="/" element={<Navigation />} />
+          <Route path="/flashcard" element={<Flashcard />} />
+          <Route path="/navigation" element={<Navigation />} />
+          <Route path="/pet" element={<Pet />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="*" element={<div>404 — Not found</div>} />
+        </Routes>
+      </main>
+    </Router>
+  )
+}
+
+export default App
---
+++ Aivy/src/Components/Flashcard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Flashcard = () => {
+  return (
+    <div>Flashcard</div>
+  )
+}
